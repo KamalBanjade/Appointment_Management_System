f35972de1213f70a07de4f4f3cc9ad46ec2facc2
Kamal Banjade <kamalbanjade67@gmail.com>
2025-02-01 13:18:24 +0545

    customized notification

---
+++ src/toastConfig.js
@@ -0,0 +1,14 @@
+// toastConfig.js
+export const toastConfig = {
+    autoClose: 3000,
+    className: "text-xs sm:text-sm lg:text-base", // Responsive text size
+    bodyClassName: "p-2 sm:p-3 lg:p-4", // Responsive padding
+    style: {
+      minWidth: "200px", // Minimum width for the toast
+      maxWidth: "70%", // Maximum width for the toast
+      margin: "8px", // Margin for spacing
+      borderRadius: "8px", // Rounded corners
+      fontSize: "0.75rem", // Base font size (text-xs)
+    },
+  };
+  
