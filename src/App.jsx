e063913ffef4c61519b1c6ea645dd74d24087140
Kamal Banjade <kamalbanjade67@gmail.com>
2025-02-01 13:18:54 +0545

    customized notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import MyRoutes from "./MyRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
-import store, { persistor } from './Redux/Store'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './Redux/Store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from "react-toastify";
-
+import { toastConfig } from './toastConfig'; 
 
 const App = () => {
   return (
     <div>
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer 
+        position="top-right" 
+        {...toastConfig} 
+      />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Router>
